70152b3c26239c8e3f97f4d31676e2e079e42508
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2023-07-28 14:55:29 +0100

    Connected to databse

---
+++ Data/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolManagement.MVC.Data;
+
+public partial class Client
+{
+    public int Id { get; set; }
+
+    public string Prenom { get; set; } = null!;
+
+    public string Nom { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string Adress { get; set; } = null!;
+
+    public string Ville { get; set; } = null!;
+
+    public DateTime DateNaissance { get; set; }
+}
---
+++ Data/ThreeSixtyDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagement.MVC.Data;
+
+public partial class ThreeSixtyDbContext : DbContext
+{
+    public ThreeSixtyDbContext()
+    {
+    }
+
+    public ThreeSixtyDbContext(DbContextOptions<ThreeSixtyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost, 1400;Database=ThreeSixtyDB;Trusted_Connection=false;MultipleActiveResultSets=true;Encrypt=false;TrustServerCertificate=true;User id=sa;Password=RayanRayan@0");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clients__3213E83FDB9F58AF");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Adress)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("adress");
+            entity.Property(e => e.DateNaissance).HasColumnType("date");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nom");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("phone");
+            entity.Property(e => e.Prenom)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("prenom");
+            entity.Property(e => e.Ville)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("ville");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
