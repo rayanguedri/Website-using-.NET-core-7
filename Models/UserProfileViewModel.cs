ee77d380c024379f43435ed3b612fc977ebab077
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2023-08-12 11:57:15 +0100

    Profile

---
+++ Models/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagement.MVC.Models
+{
+    public class UserProfileViewModel
+    {
+        public string? Name { get; internal set; }
+        public string? EmailAddress { get; internal set; }
+        public string? ProfileImage { get;internal set; }
+    }
+}
